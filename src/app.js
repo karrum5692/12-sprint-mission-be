3ec501d177317ec70691f3e2ad7c2ba035eb26b0
karrum5692 <ravee569078@gmail.com>
2026-05-16 02:23:18 +0900

    express 용량 수정

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use("/products", productRouter);
 app.use("/articles", articleRouter);
